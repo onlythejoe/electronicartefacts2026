094d3f8a7184ea1893de81b291a8c2cb58cd0c1e
onlythejoe <marquetjoey@gmail.com>
2026-07-09 13:27:09 +0200

    Configure GA4 measurement ID

--- assets/js/app.js
+++ assets/js/app.js
@@ -17458,7 +17458,7 @@ window.EA_SEARCH = {
 window.EA_ANALYTICS_CONFIG = {
   enabled: true,
   provider: "Google Analytics 4",
-  measurementId: "",
+  measurementId: "G-67DKY05YS7",
   privacyUrl: "/confidentialite.html",
 };
 
--- assets/js/core/analytics-config.js
+++ assets/js/core/analytics-config.js
@@ -1,6 +1,6 @@
 window.EA_ANALYTICS_CONFIG = {
   enabled: true,
   provider: "Google Analytics 4",
-  measurementId: "",
+  measurementId: "G-67DKY05YS7",
   privacyUrl: "/confidentialite.html",
 };
